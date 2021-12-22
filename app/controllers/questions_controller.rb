8a796dc6fdf42881cc03024fd5ecde7504f7261d
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2021-12-22 16:38:24 +0900

    action new, create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,24 @@ class QuestionsController < ApplicationController
   def index
     @test = "テスト"
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to("/questions/new")
+      flash[:notice] = "成功!"
+    else
+      flash.now[:alert] = "失敗!"
+      render("questions/new")
+    end
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/questions", to: "questions#index"
+  get "questions/new", to: "questions#new"
+  post "/questions", to: "questions#create"
 end
